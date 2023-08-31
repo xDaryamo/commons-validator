8d39d18dab6cf87e4b6b7a490d1a2af18c49daf3
Xzeni <n.delogu@studenti.unisa.it>
2023-09-01 01:03:13 +0200

    Refactored class methods to improve eco-design: used ++i instead of i++, avoided the use of arrays in for each loop, simplified return statement
    Affected files:
    - UrlValidator

--- src/main/java/org/apache/commons/validator/routines/UrlValidator.java
+++ src/main/java/org/apache/commons/validator/routines/UrlValidator.java
@@ -277,13 +277,15 @@ public class UrlValidator implements Serializable {
                 schemes = DEFAULT_SCHEMES;
             }
             allowedSchemes = new HashSet<>(schemes.length);
-            for (final String scheme : schemes) {
+            for (int i = 0; i < schemes.length; ++i) {
+                final String scheme = schemes[i];
                 allowedSchemes.add(scheme.toLowerCase(Locale.ENGLISH));
             }
         }
 
         this.authorityValidator = authorityValidator;
     }
+
 
     /**
      * <p>Checks if a field has a valid URL address.</p>
@@ -333,11 +335,7 @@ public class UrlValidator implements Serializable {
             return false;
         }
 
-        if (!isValidFragment(uri.getRawFragment())) {
-            return false;
-        }
-
-        return true;
+        return isValidFragment(uri.getRawFragment());
     }
 
     /**
@@ -358,11 +356,7 @@ public class UrlValidator implements Serializable {
             return false;
         }
 
-        if (isOff(ALLOW_ALL_SCHEMES) && !allowedSchemes.contains(scheme.toLowerCase(Locale.ENGLISH))) {
-            return false;
-        }
-
-        return true;
+        return !isOff(ALLOW_ALL_SCHEMES) || allowedSchemes.contains(scheme.toLowerCase(Locale.ENGLISH));
     }
 
     /**
@@ -471,11 +465,7 @@ public class UrlValidator implements Serializable {
         }
 
         final int slash2Count = countToken("//", path);
-        if (isOff(ALLOW_2_SLASHES) && (slash2Count > 0)) {
-            return false;
-        }
-
-        return true;
+        return !isOff(ALLOW_2_SLASHES) || (slash2Count <= 0);
     }
 
     /**
@@ -516,8 +506,8 @@ public class UrlValidator implements Serializable {
         while (tokenIndex != -1) {
             tokenIndex = target.indexOf(token, tokenIndex);
             if (tokenIndex > -1) {
-                tokenIndex++;
-                count++;
+                ++tokenIndex;
+                ++count;
             }
         }
         return count;
