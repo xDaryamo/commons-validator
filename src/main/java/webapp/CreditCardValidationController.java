2a804f2e08cbf9b2d7d6460a079a49492c4c29ee
xDaryamo <mr.dario.mazza@gmail.com>
2023-09-07 19:50:18 +0200

    Updated POM

--- src/main/java/webapp/CreditCardValidationController.java
+++ src/main/java/webapp/CreditCardValidationController.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.apache.commons.validator.routines.CreditCardValidator;
+
 
 @Controller
 public class CreditCardValidationController {
